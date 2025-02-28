73e6148b4cbeded54a1d7a61028dc9dbb2f70f15
unnhe <22521045@gm.uit.edu.vn>
2025-02-28 12:41:35 +0700

    Update Login, Signup

--- wp-content/themes/literead/Signup.php
+++ wp-content/themes/literead/Signup.php
@@ -7,7 +7,7 @@ get_header();
   
   <div class="flex overflow-hidden flex-col w-full bg-red-100">
    
-  <div class="flex flex-col px-[17px] pt-[17px] w-full text-[2.25rem] text-red-dark bg-white min-h-[779px]">
+  <div class="flex flex-col px-[17px] pt-[17px] w-full text-[18px] text-red-dark bg-white min-h-[779px]">
     <form>
       <div class="flex flex-col w-full tracking-wide leading-none">
         <label for="emailOrPhone" class="font-semibold">Email hoặc số điện thoại</label>
@@ -19,7 +19,6 @@ get_header();
         <label for="password" class="font-semibold">Mật khẩu</label>
         <div class="flex overflow-hidden gap-1.5 items-center px-[8px] py-[12px] mt-[8px] w-full whitespace-nowrap border-b border-solid border-red-dark">
           <input type="password" id="password" placeholder="**********" class="flex-1 shrink self-stretch my-auto opacity-60 basis-0 bg-transparent border-none outline-none" />
-
         </div>
       </div>
       <div class="flex flex-col mt-[12px] w-full tracking-wide leading-none">
@@ -28,7 +27,7 @@ get_header();
           <input type="password" id="confirmPassword" placeholder="**********" class="flex-1 shrink self-stretch my-auto opacity-60 basis-0 bg-transparent border-none outline-none" />
         </div>
       </div>
-      <div class="mt-[12px] w-full] text-[2rem] font-medium text-center text-stone-500">
+      <div class="mt-[12px] w-full] text-[16px] font-medium text-center text-stone-500">
         <span class="text-red-dark ">Bạn đã có tài khoản?</span>
         <a href="#" class="font-semibold text-red-dark-hover">Đăng nhập</a>
       </div>
