b6c710e68d923d0d1c49b906da111b6422121496
pthao207203 <149165346+pthao207203@users.noreply.github.com>
2025-03-06 19:37:59 +0700

    update

--- wp-content/themes/literead/footer.php
+++ wp-content/themes/literead/footer.php
@@ -3,6 +3,8 @@
     <p>&copy; <?php echo date('Y'); ?> from 6 con gắn. All Rights Reserved.</p>
   </div>
 </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
 <script>
   var quill = new Quill('#synopsis', {
