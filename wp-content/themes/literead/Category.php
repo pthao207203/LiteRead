340d416baba9d226d50a16f427c341daa3265ed8
Trần Lâm Ngọc Khanh <me123lac@gmail.com>
2025-03-11 20:32:06 +0700

    Add Searching

---
+++ wp-content/themes/literead/Category.php
@@ -0,0 +1,97 @@
+<?php
+/* Template Name: Category */
+get_header(); 
+?>
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css"
+/>
+<link
+  href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Moul&display=swap"
+  rel="stylesheet"
+/>
+<div class="mx-auto w-full max-w-none min-h-screen bg-white max-md:max-w-[114.25rem]">
+    <main class="px-14 py-9 bg-white max-md:px-[2rem] max-md:py-[1.25rem]">
+      <div class="flex flex-col gap-[1rem]">
+        <h2 class="text-3xl font-semibold tracking-wide leading-6 text-[#A04D4C]">
+          Thể loại
+        </h2>
+        <div class="flex flex-wrap gap-[1rem] justify-center items-start max-md:gap-3">
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            ABO
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Mạt thế
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Ngọt sủng
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Ngược
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Ngôn tình
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Đam mỹ
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Bách hợp
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            SE
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            OE
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            HE
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Cổ đại
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Dân quốc
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Hiện đại
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Xuyên không
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Trọng sinh
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Hệ thống
+          </button>
+          <button class="px-[0.625rem] py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Nữ cường
+          </button>
+          <button class="px-[0.5rem].5 py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Tổng tài
+          </button>
+          <button class="px-[0.5rem].5 py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Thế thân
+          </button>
+          <button class="px-[0.5rem].5 py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Tu tiên
+          </button>
+          <button class="px-[0.5rem].5 py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Nam chủ
+          </button>
+          <button class="px-[0.5rem].5 py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Truyện ngắn
+          </button>
+          <button class="px-[0.5rem].5 py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Truyện dài
+          </button>
+          <button class="px-[0.5rem].5 py-[1.25rem] text-2xl text-[#803D3D] bg-[#FFE5E1] rounded-[0.5rem] cursor-pointer w-[11.25rem] max-md:px-[0.5rem] max-md:py-[1rem] max-md:text-xl max-md:w-[9.375rem]">
+            Đã FULL
+          </button>
+        </div>
+      </div>
+    </main>
+</div>
+<?php get_footer(); ?>
