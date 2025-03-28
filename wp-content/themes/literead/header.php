457598d2204681edc9b8085c660cb708357ea950
tuongvy <22521701@gm.uit.edu.vn>
2025-03-28 17:07:42 +0700

    fix Dieu-khoan

--- wp-content/themes/literead/header.php
+++ wp-content/themes/literead/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php bloginfo('name'); ?><?php wp_title('|', true, 'left'); ?></title>
+  <title><?php bloginfo('name'); ?> | <?php wp_title('', true, 'left'); ?></title>
   <?php wp_head(); ?>
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
